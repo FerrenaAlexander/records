588efda327e8734efb5775a61b2d668c134a431c
Alex Ferrena <apf2139@columbia.edu>
2018-03-23 13:01:17 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="records",
+    version="0.1.1",
+    packages=find_packages(),
+    author="Alex Ferrena",
+    license="GPLv3",
+    description="A package for downloading from GFIB REST API",
+    classifiers=["Programming Language :: Python :: 3"],
+)
